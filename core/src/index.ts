8a2739115d6eff708bb7d1aa4923c526855d06a8
Mohamed El Dahtowry <salintofficial@hotmail.com>
2024-08-06 04:38:15 +0300

    Setup Express and dotenv

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world")
+import { config as env } from "dotenv"
+import express from "express"
+
+if(process.env.NODE_ENV !== "production") {
+    env();
+}
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(process.env.PORT)
