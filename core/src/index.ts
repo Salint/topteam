e5fa07cda4f2d3b98a162b78a4693a04d01f8c82
Mohamed El Dahtowry <salintofficial@hotmail.com>
2024-08-06 04:59:26 +0300

    Add middleware

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,14 +1,18 @@
-import { config as env } from "dotenv"
-import express from "express"
-
+import { config as env } from "dotenv";
 if(process.env.NODE_ENV !== "production") {
     env();
 }
 
-const app = express()
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-})
+});
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT);
